5b530261a0fa61381dad09e320af03a55edf2815
asif256000 <asif256000@gmail.com>
2026-01-19 21:12:06 -0500

    refactor: update public exports to use new CNN module and remove deprecated trainer

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,13 @@
-"""Public exports for phylogenetic ML models (currently the CNN module)."""
+"""Public exports for phylogenetic ML models (updated CNN + KAN)."""
 
-from .cnn import (
-    CNNModel,
-    CNNTrainer,
-    ConfigurationError,
-    TrainingConfig,
-    load_training_config,
-)
+from .updated_cnn import CNNModel, Trainer, TrainingResult, run_training
+from .updated_cnn.config import ConfigurationError, TrainingConfig, load_training_config
 
 __all__ = [
     "CNNModel",
-    "CNNTrainer",
+    "Trainer",
+    "TrainingResult",
+    "run_training",
     "ConfigurationError",
     "TrainingConfig",
     "load_training_config",
